ba8e6490b7ff83aa7a97bb4402a67c580d2747eb
Jan Biehl <jpbiehl@googlemail.com>
2023-05-22 19:58:59 +0200

    Corrected namespace

--- TiaGenerator/FileManager.cs
+++ TiaGenerator/FileManager.cs
@@ -3,39 +3,40 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace TiaGenerator;
-
-public static class FileManager
+namespace TiaGenerator
 {
-    private static readonly HashSet<string> Files = new();
-    private static readonly HashSet<string> Directories = new();
-    
-    public static void RegisterDirectory(string directory)
-    {
-        if (string.IsNullOrWhiteSpace(directory))
-            throw new ArgumentException("Value cannot be null or whitespace.", nameof(directory));
-        
-        Directories.Add(directory);
-    }
-    
-    public static void RegisterFile(string filePath)
-    {
-        if (string.IsNullOrWhiteSpace(filePath))
-            throw new ArgumentException("Value cannot be null or whitespace.", nameof(filePath));
+	public static class FileManager
+	{
+		private static readonly HashSet<string> Files = new();
+		private static readonly HashSet<string> Directories = new();
 
-        Files.Add(filePath);
-    }
+		public static void RegisterDirectory(string directory)
+		{
+			if (string.IsNullOrWhiteSpace(directory))
+				throw new ArgumentException("Value cannot be null or whitespace.", nameof(directory));
 
-    public static void Cleanup()
-    {
-        foreach (var file in Files.Where(File.Exists))
-        {
-            File.Delete(file);
-        }
+			Directories.Add(directory);
+		}
 
-        foreach (var directory in Directories)
-        {
-            Directory.Delete(directory, true);
-        }
-    }
+		public static void RegisterFile(string filePath)
+		{
+			if (string.IsNullOrWhiteSpace(filePath))
+				throw new ArgumentException("Value cannot be null or whitespace.", nameof(filePath));
+
+			Files.Add(filePath);
+		}
+
+		public static void Cleanup()
+		{
+			foreach (var file in Files.Where(File.Exists))
+			{
+				File.Delete(file);
+			}
+
+			foreach (var directory in Directories)
+			{
+				Directory.Delete(directory, true);
+			}
+		}
+	}
 }
