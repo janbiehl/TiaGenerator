519c49d6b11aefe7d1a9fc6d82e430279ae0f685
Jan Biehl <jpbiehl@googlemail.com>
2023-05-22 21:41:16 +0200

    Introduced action for a calling FC block generation

---
+++ TiaGenerator/Actions/PlcActions/BlockActions/CreateCallingFC.cs
@@ -0,0 +1,156 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Siemens.Engineering;
+using Siemens.Engineering.SW;
+using TiaGenerator.Core.Interfaces;
+using TiaGenerator.Core.Models;
+using TiaGenerator.Models;
+using TiaGenerator.Templates;
+using TiaGenerator.Tia;
+using TiaGenerator.Tia.Extensions;
+using TiaGenerator.Tia.Utils;
+using TiaGenerator.Utils;
+
+namespace TiaGenerator.Actions
+{
+	/// <summary>
+	/// This action creates a FC that calls every block that is contained in a specific block group
+	/// </summary>
+	public class CreateCallingFC : GeneratorAction
+	{
+		/// <summary>
+		/// The block group that contains the blocks that should be called. Separated by '/'.
+		/// </summary>
+		/// <remarks>This works for FC and FB block calls</remarks>
+		public string? TargetBlockGroup { get; set; }
+
+		/// <summary>
+		/// The block group the calling FC should be created in
+		/// </summary>
+		public string? BlockGroup { get; set; }
+
+		/// <summary>
+		/// The name that the generated FC should have
+		/// </summary>
+		public string? BlockName { get; set; }
+
+		/// <summary>
+		/// The author that will be used for the generated block
+		/// </summary>
+		public string? Author { get; set; }
+
+		/// <summary>
+		/// The family that will be used for the generated block
+		/// </summary>
+		public string? Family { get; set; }
+
+		/// <summary>
+		/// The number that will be used for the generated block
+		/// </summary>
+		public int BlockNumber { get; set; }
+
+		/// <summary>
+		/// Whether the block should be automatically numbered or not
+		/// </summary>
+		public bool AutoNumber { get; set; }
+
+		/// <inheritdoc />
+		public override Task<ActionResult> Execute(IDataStore datastore)
+		{
+			if (string.IsNullOrWhiteSpace(TargetBlockGroup))
+				return Task.FromResult(new ActionResult(ActionResultType.Failure, "No target block group specified."));
+
+			if (string.IsNullOrWhiteSpace(BlockGroup))
+				return Task.FromResult(new ActionResult(ActionResultType.Failure, "No block group specified."));
+
+			if (string.IsNullOrWhiteSpace(BlockName))
+				return Task.FromResult(new ActionResult(ActionResultType.Failure, "No block name specified."));
+
+			if (string.IsNullOrWhiteSpace(Author))
+				return Task.FromResult(new ActionResult(ActionResultType.Failure, "No author specified."));
+
+			if (string.IsNullOrWhiteSpace(Family))
+				return Task.FromResult(new ActionResult(ActionResultType.Failure, "No family specified."));
+
+			if (BlockNumber < 1)
+				return Task.FromResult(new ActionResult(ActionResultType.Failure,
+					"Invalid block number -> It may not be 0 or less"));
+
+			if (datastore is not DataStore dataStore)
+			{
+				throw new System.InvalidOperationException("Invalid datastore");
+			}
+
+			var plcDevice = dataStore.TiaPlcDevice ??
+			                throw new System.InvalidOperationException("There is no plc device in the data store.");
+
+			var targetBlockGroup = PlcSoftwareUtils.GetBlockGroup(plcDevice.PlcSoftware, TargetBlockGroup.Split('/'));
+
+			if (targetBlockGroup is null)
+				return Task.FromResult(new ActionResult(ActionResultType.Failure,
+					$"Block group '{TargetBlockGroup}' does not exist."));
+
+			var networks = new List<string>();
+
+			// Get the informations about the blocks inside the block group
+			foreach (var block in targetBlockGroup.Blocks)
+			{
+				var blockType = block.GetBlockType();
+				string? blockContent = null;
+
+				switch (blockType)
+				{
+					case BlockType.Fb:
+					{
+						TemplateFbBlockCall fbBlockCall = new()
+						{
+							BlockName = block.Name,
+							BlockInstanceName = $"iDB_{block.Name}"
+						};
+
+						networks.Add(fbBlockCall.TransformText());
+						break;
+					}
+					case BlockType.Fc:
+					{
+						TemplateFcBlockCall dummy = new()
+						{
+							BlockName = block.Name
+						};
+
+						networks.Add(dummy.TransformText());
+						break;
+					}
+				}
+			}
+
+			TemplateBlockFC blockFc = new()
+			{
+				BlockName = BlockName,
+				Author = Author,
+				Family = Family,
+				AutoNumber = AutoNumber,
+				BlockNumber = BlockNumber,
+				Networks = networks
+			};
+
+			var tempBlockFilePath = PathUtils.GetBlockFilePath(PathUtils.ApplicationTempDirectory, BlockName);
+			FileManager.RegisterFile(tempBlockFilePath);
+
+			File.WriteAllText(tempBlockFilePath, blockFc.TransformText());
+
+			var blockGroup = dataStore.TiaPlcDevice.PlcSoftware.GetOrCreateGroup(BlockGroup.Split('/'));
+			var importedBlocks = blockGroup.Blocks.ImportBlocksFromFile(tempBlockFilePath, ImportOptions.None,
+				SWImportOptions.IgnoreStructuralChanges | SWImportOptions.IgnoreUnitAttributes |
+				SWImportOptions.IgnoreMissingReferencedObjects);
+
+			if (importedBlocks.Count > 0)
+				return Task.FromResult(new ActionResult(ActionResultType.Success,
+					$"FC '{BlockName}' created in block group '{BlockGroup}', invoking the blocks from {TargetBlockGroup}"));
+
+			return Task.FromResult(new ActionResult(ActionResultType.Failure,
+				$"FC '{BlockName}' could not be created in block group '{BlockGroup}'"));
+		}
+	}
+}
