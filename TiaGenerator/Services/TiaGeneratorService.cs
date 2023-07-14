8abbb68d613e7cf3debd8d6c9b446d80ed2f102c
Jan Biehl <jpbiehl@googlemail.com>
2023-07-14 23:21:28 +0200

    Restructured generation

--- TiaGenerator/Services/TiaGeneratorService.cs
+++ TiaGenerator/Services/TiaGeneratorService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
@@ -8,14 +9,15 @@ using TiaGenerator.Models;
 
 namespace TiaGenerator.Services
 {
-	public class TiaGeneratorService : BackgroundService
+	internal sealed class TiaGeneratorService : BackgroundService
 	{
 		private readonly ILogger<TiaGeneratorService> _logger;
 		private readonly Options _options;
 		private readonly DataProviderService _dataProvider;
 		private readonly IHostApplicationLifetime _applicationLifetime;
 
-		public TiaGeneratorService(ILogger<TiaGeneratorService> logger, Options options, DataProviderService dataProvider
+		public TiaGeneratorService(ILogger<TiaGeneratorService> logger, Options options,
+			DataProviderService dataProvider
 			, IHostApplicationLifetime applicationLifetime)
 		{
 			_logger = logger;
@@ -44,54 +46,62 @@ namespace TiaGenerator.Services
 			}
 			else
 			{
-				_logger.LogError("No data loaded.");
+				_logger.LogDebug("Configuration loaded {@Data}", configuration);
+
+				await ExecuteActions(configuration.Actions, stoppingToken);
+
+				// Cleanup any temporary files or directories
+				if (_options.Cleanup)
+					await FileManager.Cleanup(stoppingToken);
 			}
-
-			_logger.LogDebug("Data loaded {@Data}", data);
-
-			using var dataStore = new DataStore();
-
-			if (data?.Actions != null)
-			{
-				foreach (var action in data.Actions)
-				{
-					if (stoppingToken.IsCancellationRequested)
-					{
-						_logger.LogInformation("Cancellation requested. Stopping.");
-						break;
-					}
-					
-					try
-					{
-						var result = await action.Execute(dataStore);
-
-						switch (result.Result)
-						{
-							case ActionResultType.Failure:
-								_logger.LogError(result.Message);
-								break;
-							case ActionResultType.Fatal:
-								_logger.LogCritical(result.Message);
-								break;
-							case ActionResultType.Success:
-								_logger.LogInformation(result.Message);
-								break;
-							default:
-								throw new ArgumentOutOfRangeException(nameof(result.Result), "The result is unknown");
-						}
-					}
-					catch (Exception e)
-					{
-						_logger.LogCritical(e, "Could not execute action {Action}", action);
-						break; // Leave the loop as we had a fatal error
-					}
-				}
-			}
-
-			if (_options.Cleanup)
-				await FileManager.Cleanup(stoppingToken);
 
 			_applicationLifetime.StopApplication();
+		}
+
+		/// <summary>
+		/// Execute any actions that are read from configuration
+		/// </summary>
+		/// <param name="actions">Actions that will be used</param>
+		/// <param name="cancellationToken">Will cancel the operation while it is running</param>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
+		private async Task ExecuteActions(IEnumerable<GeneratorAction> actions, CancellationToken cancellationToken)
+		{
+			// The datastore is used to store data between actions
+			using var dataStore = new DataStore();
+
+			foreach (var action in actions)
+			{
+				if (cancellationToken.IsCancellationRequested)
+				{
+					_logger.LogInformation("Cancellation requested. Stopping.");
+					cancellationToken.ThrowIfCancellationRequested();
+				}
+
+				try
+				{
+					var result = await action.Execute(dataStore);
+
+					switch (result.Result)
+					{
+						case ActionResultType.Failure:
+							_logger.LogError(result.Message);
+							break;
+						case ActionResultType.Fatal:
+							_logger.LogCritical(result.Message);
+							break;
+						case ActionResultType.Success:
+							_logger.LogInformation(result.Message);
+							break;
+						default:
+							throw new ArgumentOutOfRangeException(nameof(result.Result), "The result is unknown");
+					}
+				}
+				catch (Exception e)
+				{
+					_logger.LogCritical(e, "Could not execute action {Action}", action);
+					break; // Leave the loop as we had a fatal error
+				}
+			}
 		}
 	}
 }
